e0efae65c4136e55a09ef20e21e03ef137d63546
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-06-12 22:38:12 +0300

    add resprctive changes to index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 31
+let num2 = 2
+function multiply(num1, num2) {
+    return num1*num2
+}
 
-// Write your code here
+let num3 = 85
+let num4 = 9
+function mod (num3, num4) {
+    return num3 % num4
+}
+
+let num5 = 15
+let num6 = 20
+let num7 = 1
+let num8 = 7
+function max(num5,num6,num7,num8) {
+    return (Math.max(num5,num6,num7,num8))
+}
+
+console.log(multiply (31,2))
+console.log(mod(num3,num4))
+console.log(max(num5,num6,num7,num8))
+
